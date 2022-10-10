c38c17bf0a4a2f6fb30ffc41e61c9c092a646ce1
Majharul-Munna <majharulmunna21@gmail.com>
2022-10-11 02:49:06 +0600

    404 page added

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <div className=' bg-slate-300 '>
+            <Link className=' text-2xl font-semibold text-white m-4' to='/home'>Home</Link>
+            <Link className=' text-2xl font-semibold text-white m-4 py-7' to='/topics'>Topics</Link>
+            <Link className=' text-2xl font-semibold text-white m-4 py-7' to='/statics'>Statics</Link>
+            <Link className=' text-2xl font-semibold text-white m-4 py-7' to='/blog'>Blog</Link>
+        </div>
+    );
+};
+
+export default NavBar;
